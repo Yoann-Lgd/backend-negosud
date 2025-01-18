d615f9f57e378f42ec11bfea01003add4fd99649
Pimer0 <zefun74@gmail.com>
2025-01-18 23:24:09 +0100

    Ajout de fluent validator pour verification supplémentaire du format de donnée saisie

---
+++ backend-negosud/Validation/UtilisateurValidation.cs
@@ -0,0 +1,20 @@
+using backend_negosud.DTOs;
+using FluentValidation;
+
+namespace backend_negosud.Validation;
+
+public class UtilisateurValidation : AbstractValidator<UtilisateurInputDto>
+{
+    public UtilisateurValidation()
+    {
+        RuleFor(utilisateur => utilisateur.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .WithMessage("Saisir une adresse mail valide");
+
+        RuleFor(utilisateur => utilisateur.MotDePasse)
+            .NotEmpty()
+            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$")
+            .WithMessage("Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial, et faire au moins 8 caractères.");
+    }
+}
