6e07107841ed1780108ed35cef4d88d40fbb7740
BuzzLeclair <yoann.lengrand@icloud.com>
2025-02-01 19:29:24 +0100

    - Migration qui permet de supprimer la colonne quantite qui ne sert pas à grand chose puisque la quantite est présente au sein de la ligne de commande

---
+++ backend-negosud/Migrations/20250201181656_DeleteQuantiteInCommande.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_negosud.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteQuantiteInCommande : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "quantite",
+                table: "Commande");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "quantite",
+                table: "Commande",
+                type: "int",
+                nullable: false,
+                defaultValue: 0); 
+        }
+    }
+}
