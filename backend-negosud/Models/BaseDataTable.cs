992f7fb108cc69a1fa12a61e1ec6e134feb5b946
Pimer0 <zefun74@gmail.com>
2025-02-08 23:26:05 +0100

    fix validation email

--- backend-negosud/Models/BaseDataTable.cs
+++ backend-negosud/Models/BaseDataTable.cs
@@ -4,15 +4,14 @@ namespace backend_negosud.Models;
 
 public class BaseDataTable : DataTable
 {
-
     public BaseDataTable(string tableName) : base(tableName)
     {
         SetupColumns();
     }
+    
     protected virtual void SetupColumns()
     {
         this.Columns.Add("ID", typeof(int));
-        this.Columns.Add("codeEmail", typeof(string));
     }
     
     public void SetPrimaryKeyColumn(string columnName)
