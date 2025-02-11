a3cf06b6e1c52b09753770c66c9cc42d10308088
BuzzLeclair <yoann.lengrand@icloud.com>
2025-02-12 00:11:51 +0100

    - Modification de la fonctioon updatePanier, maintenant elle prend en paramètre PanierUpdateInputDto qui comporte commandeId, quantité et articleID - Si la quantité est à 0 la ligne de commande est supprimée.

--- backend-negosud/DTOs/Commande-client/Inputs/PanierUpdateInputDto.cs
+++ backend-negosud/DTOs/Commande-client/Inputs/PanierUpdateInputDto.cs
@@ -3,6 +3,6 @@ namespace backend_negosud.DTOs.Commande_client;
 public class PanierUpdateInputDto
 {
     public int CommandId { get; set; }
-    public int ClientId { get; set; }
-    public List<LigneCommandeUpdateInputDto> LigneCommandes { get; set; } = new List<LigneCommandeUpdateInputDto>();
+    public int ArticleId { get; set; }
+    public int NewQuantite { get; set; }
 }
