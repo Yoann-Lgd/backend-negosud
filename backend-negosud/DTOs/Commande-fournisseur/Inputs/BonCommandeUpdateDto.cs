d8a1920a5455fe3abb90ca81fd711a4e847bf031
BuzzLeclair <yoann.lengrand@icloud.com>
2025-03-14 14:54:37 +0100

    - Liaison entre BonCommande et Fournisseur - Incrémentation de la fonctionnalité de restock lorsque toutes les ligneBonCommande ont le booléen livree a true et que le statut de BonCommande est à Livrée

--- backend-negosud/DTOs/Commande-fournisseur/Inputs/BonCommandeUpdateDto.cs
+++ backend-negosud/DTOs/Commande-fournisseur/Inputs/BonCommandeUpdateDto.cs
@@ -4,4 +4,5 @@ public class BonCommandeUpdateDto
 {
     public string Status { get; set; } = null!;
     public List<LigneBonCommandeUpdateDto> LigneCommandes { get; set; }
+    public int UtilisateurId { get; set; }
 }
